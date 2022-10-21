8666af37c2183916bf1ff32a026762c0beba9952
Tomas Daneri <tfdaneri@gmail.com>
2022-10-20 22:46:12 -0300

    listo

---
+++ ListaDeContactos.js
@@ -0,0 +1,6 @@
+let contactos = ["Pedro Perez", "Pablo Pablez", "Fer Fernandez"];
+console.log(contactos);
+let nuevaLongitud = contactos.push("Gonzalo Gonzalez");
+console.log(contactos)
+let ultimo = contactos.pop()
+console.log(contactos)
